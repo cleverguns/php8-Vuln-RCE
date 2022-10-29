dca4e13071de83a8ff9fdf26ecc9353ffc3ee2b4
Carlo G <41587193+cleverguns@users.noreply.github.com>
2022-10-29 20:27:04 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# php8-Vuln-RCE
+ PHP 8 vulnerability - rce, will do a video demo (tagalog version)
---
+++ php8-RCE-backdoor.php
@@ -0,0 +1,9 @@
+/** 
+https://github.com/vulhub/vulhub/blob/master/php/8.1-backdoor/README.md
+https://flast101.github.io/php-8.1.0-dev-backdoor-rce/
+
+*/
+
+
+<?php
+echo "backdoor sample";
